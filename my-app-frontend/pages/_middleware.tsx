4a49f48679eb3ce5d15a3abd0cdf8d704efca633
Guillermo López García <lopezgarciaguillermo@live.com>
2021-11-14 14:24:04 +0100

    add singles quotes to editorconfig

--- my-app-frontend/pages/_middleware.tsx
+++ my-app-frontend/pages/_middleware.tsx
@@ -1,14 +1,14 @@
-import { NextRequest, NextResponse, NextFetchEvent } from "next/server";
+import { NextRequest, NextResponse, NextFetchEvent } from 'next/server';
 
-import { isSigned } from "../src/services/is-signed";
-import { availablePathWithoutLogin } from "../src/services/available-paths-without-login";
+import { isSigned } from '../src/services/is-signed';
+import { availablePathWithoutLogin } from '../src/services/available-paths-without-login';
 
 export const middleware = (req: NextRequest, ev: NextFetchEvent) => {
   if (!isSigned()) {
     if (!availablePathWithoutLogin(req.nextUrl.pathname)) {
-      return NextResponse.redirect("/login");
+      return NextResponse.redirect('/login');
     }
   } else {
-    return NextResponse.redirect("/home");
+    return NextResponse.redirect('/home');
   }
 };
